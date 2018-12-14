ad32effdde06b8123b2323cad954f97f0799eeed
Joshie Fishbein <joshie@sanctuary.computer>
2018-12-14 17:08:16 -0500

    init repo with rollup and a package.json

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  external: Object.keys(pkg.dependencies),
+  output: {
+    file: pkg.main,
+    format: 'cjs',
+  },
+  plugins: [
+    resolve({
+      module: true,
+      main: true,
+    }),
+    babel({
+      babelrc: false,
+      presets: [['env', { modules: false }]],
+      plugins: ['transform-class-properties', 'transform-object-rest-spread', 'external-helpers'],
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
