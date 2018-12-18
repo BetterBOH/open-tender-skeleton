779266463729beafbb9cef95ca7ecff9ccbeb2f0
Joshie Fishbein <joshie@sanctuary.computer>
2018-12-18 16:34:03 -0500

    add dynamic scss flow

--- mock/src/index.js
+++ mock/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'index.scss';
 import { Skeleton } from 'open-tender-skeleton';
 
 ReactDOM.render(<Skeleton />, document.getElementById('root'));
