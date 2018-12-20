141f628a76f9fade7093bad4c3072d65d0f0dda2
Joshie Fishbein <joshie@sanctuary.computer>
2018-12-20 15:35:45 -0500

    move Context.Consumer to renderPresentation to simplify wrapper even further

--- src/components/Text/index.js
+++ src/components/Text/index.js
@@ -1,9 +1,3 @@
-import React from 'react';
-import { ConfigContext } from 'config';
 import renderPresentation from 'utils/renderPresentation';
 
-export default props => (
-  <ConfigContext.Consumer>
-    {context => renderPresentation(context, props, 'Text')}
-  </ConfigContext.Consumer>
-);
+export default props => renderPresentation(props, 'Text');
--- src/utils/renderPresentation.js
+++ src/utils/renderPresentation.js
@@ -1,19 +1,25 @@
 import React from 'react';
+
+import { ConfigContext } from 'config';
 import get from 'utils/get';
 
-export default (context, props, displayName) => {
-  const PrimaryPresentation = React.lazy(() =>
-    import(`components/${displayName}/presentation`)
-  );
+export default (props, displayName) => (
+  <ConfigContext.Consumer>
+    {context => {
+      const PrimaryPresentation = React.lazy(() =>
+        import(`components/${displayName}/presentation`)
+      );
 
-  const AlternatePresentation = get(
-    context,
-    `registry.components.${displayName}.component`
-  );
+      const AlternatePresentation = get(
+        context,
+        `registry.components.${displayName}.component`
+      );
 
-  return AlternatePresentation ? (
-    <AlternatePresentation {...props} {...context} />
-  ) : (
-    <PrimaryPresentation {...props} {...context} />
-  );
-};
+      return AlternatePresentation ? (
+        <AlternatePresentation {...props} {...context} />
+      ) : (
+        <PrimaryPresentation {...props} {...context} />
+      );
+    }}
+  </ConfigContext.Consumer>
+);
