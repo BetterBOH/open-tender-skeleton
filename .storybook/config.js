565a4e0ad49743f4257954c5f0a9e0782b7d4dd3
Joshie Fishbein <joshie@sanctuary.computer>
2019-01-07 17:19:37 -0500

    add a11y + documentation addons for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,9 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withNotes } from '@storybook/addon-notes';
 
 function loadStories() {
   require('./stories/Text.js');
 }
 
+addDecorator(withNotes);
 configure(loadStories, module);
