b2e425829876ebcf1eb1a5022e50192dcf03ae00
Joshie Fishbein <joshie@sanctuary.computer>
2019-01-07 17:20:30 -0500

    fix husky

--- .storybook/stories/Text.js
+++ .storybook/stories/Text.js
@@ -6,18 +6,26 @@ import Text from '../../src/components/Text';
 import documentation from '../../src/components/Text/README.md';
 
 const addons = {
-  notes: { markdown: documentation },
+  notes: { markdown: documentation }
 };
 
 storiesOf('Text', module)
   .addDecorator(checkA11y)
-  .add('with h1', () => (
-    <React.Suspense fallback={<div />}>
-      <Text elem="h1">Hello World</Text>
-    </React.Suspense>
-  ), addons)
-  .add('with fallback span', () => (
-    <React.Suspense fallback={<div />}>
-      <Text>Hello World</Text>
-    </React.Suspense>
-  ), addons)
+  .add(
+    'with h1',
+    () => (
+      <React.Suspense fallback={<div />}>
+        <Text elem="h1">Hello World</Text>
+      </React.Suspense>
+    ),
+    addons
+  )
+  .add(
+    'with fallback span',
+    () => (
+      <React.Suspense fallback={<div />}>
+        <Text>Hello World</Text>
+      </React.Suspense>
+    ),
+    addons
+  );
