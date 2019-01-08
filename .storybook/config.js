b2b7d265a5b909031b4071fcb3b36f3c52fce163
Joshie Fishbein <joshie@sanctuary.computer>
2019-01-07 19:17:04 -0500

    add options, theme, branding to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,21 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withNotes } from '@storybook/addon-notes';
+import { withOptions } from '@storybook/addon-options';
+import { themes } from '@storybook/components';
+
+addDecorator(withNotes);
+
+addDecorator(
+  withOptions({
+    name: 'Open Tender Skeleton',
+    theme: {
+      ...themes.normal
+    }
+  })
+);
 
 function loadStories() {
   require('./stories/Text.js');
 }
 
-addDecorator(withNotes);
 configure(loadStories, module);
