6f0f884f99ffe72f8fe74c5613bf8da0c98ea4a8
Joshie Fishbein <joshie@sanctuary.computer>
2019-01-15 16:27:43 -0500

    temporarily delete yarn and use npm, move app init to GenericPageContainer

--- src/containers/GenericPageContainer.js
+++ src/containers/GenericPageContainer.js
@@ -2,25 +2,43 @@ import ContainerBase from 'lib/ContainerBase';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
+import { IDLE } from 'constants/Status';
+import { initializeApplication } from 'state/actions/applicationActions';
+import BrandibbleClient from 'lib/BrandibbleClient';
+import withContext from 'lib/withContext';
+
 import get from 'utils/get';
 
 class GenericPageContainer extends ContainerBase {
   view = import('views/GenericPageView');
+
+  model = () => {
+    const brandibbleConfig = get(this.props, 'config.registry.brandibble', {});
+    console.log(brandibbleConfig);
+    const Brandibble = new BrandibbleClient(brandibbleConfig);
+
+    const { applicationStatus } = this.props;
+
+    if (applicationStatus === IDLE) actions.initializeApplication(Brandibble);
+  };
 }
 
-const mapStateToProps = state => {
-  return {
-    test: get(state, 'test.tester')
-  };
-};
+const mapStateToProps = state => ({
+  applicationStatus: get(state, 'status.applicationStatus')
+});
 
-const mapDispatchToProps = dispatch => {
-  return {
-    actions: bindActionCreators({}, dispatch)
-  };
-};
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    {
+      initializeApplication
+    },
+    dispatch
+  )
+});
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(GenericPageContainer);
+export default withContext(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(GenericPageContainer)
+);
