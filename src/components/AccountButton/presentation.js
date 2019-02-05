4bbc0f05b7c9a23c85e8f871df4b78be16703fc6
Kay Mok <kaymok3@gmail.com>
2019-02-05 12:11:03 -0500

    Styling button and text

--- src/components/AccountButton/presentation.js
+++ src/components/AccountButton/presentation.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import cx from 'classnames';
 
 import { Text, Button } from 'components';
 
 const AccountButton = ({ className, icon, onClick, userIsAuthenticated }) => (
-  <Button>
-    <Text>{userIsAuthenticated ? 'Hugh' : 'Guest'}</Text>
+  <Button
+    onClick={onClick}
+    className={cx('AccountButton bg-color-light-gray radius-md', className)}
+  >
+    <div className="col-10 px_5">
+      <Text size="detail">{userIsAuthenticated ? 'Hugh' : 'Guest'}</Text>
+    </div>
   </Button>
 );
 
--- src/components/Nav/presentation.js
+++ src/components/Nav/presentation.js
@@ -7,7 +7,7 @@ const Nav = props => {
   const logo = get(props, 'brand.logoImage');
 
   return (
-    <div className="Nav relative p1 bg-color-white flex justify-between">
+    <div className="Nav relative p1 bg-color-white flex justify-between items-center">
       <Image className="h100" src={logo} />
       <AccountButton userIsAuthenticated={false} />
     </div>
