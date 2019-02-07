0ad3a3e7510f402de93b61464c7f0d167aadc2b7
Kay Mok <kaymok3@gmail.com>
2019-02-07 18:49:21 -0500

    Add brand file

---
+++ .storybook/brand.js
@@ -0,0 +1,13 @@
+export const brand = {
+  colors: {
+    'brand-color-light': '#CE0901',
+    'brand-color-dark': '#68070A',
+    black: '#22242A',
+    white: '#fff',
+    'gray-light': '#CDCDD7',
+    gray: '#8d92a3',
+    'gray-dark': '#4a4a4a',
+    'white-overlay': 'rgba(255, 255, 255, 0.9)',
+    'black-overlay': 'rgba(0, 0, 0, 0.1)'
+  }
+};
