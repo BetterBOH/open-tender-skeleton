ec23b520f91f7fc2eb4065a4984afe4432af6d06
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-07 18:14:57 -0500

    add input, event, and throttling for geocoder input

--- src/components/MapboxGeocoder/index.js
+++ src/components/MapboxGeocoder/index.js
@@ -3,9 +3,11 @@ import MapboxClient from '@mapbox/mapbox-sdk';
 import Geocoder from '@mapbox/mapbox-sdk/services/geocoding';
 
 import get from 'utils/get';
+import throttle from 'utils/throttle';
 import withMapbox from 'lib/withMapbox';
 
 import RegistryLoader from 'lib/RegistryLoader';
+import { Text } from 'components';
 
 class MapboxGeocoder extends Component {
   constructor(props) {
@@ -13,12 +15,40 @@ class MapboxGeocoder extends Component {
 
     const accessToken = get(props, 'mapbox.mapboxApiKey', '');
 
+    this.queryMapbox = throttle(this.queryMapbox, 500);
+
     this.Client = MapboxClient({ accessToken });
     this.Geocoder = Geocoder(this.Client);
   }
 
+  state = {
+    query: '',
+    results: []
+  };
+
+  handleOnChange = e => {
+    const { value } = e.target;
+    this.setState({ query: e.target.value });
+    this.queryMapbox(value);
+  };
+
+  queryMapbox = value => console.log(value);
+
   render() {
-    return <div>Mapbox Geo</div>;
+    return (
+      <div>
+        <Text size="body" className="text-bold">
+          Mapbox Geocoder
+        </Text>
+        <div className="my1">
+          <input
+            type="text"
+            onChange={this.handleOnChange}
+            value={this.state.query}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
