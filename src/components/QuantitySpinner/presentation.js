6b0e34f65110693e34de4c3272baad8c25b2a630
Kay Mok <kaymok3@gmail.com>
2019-02-13 18:20:38 -0500

    Add styling for when quantity > 0

--- src/components/QuantitySpinner/presentation.js
+++ src/components/QuantitySpinner/presentation.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import { Text } from 'components';
+import { Text, Button, Icon } from 'components';
 
 const QuantitySpinner = React.memo(props => {
   const { quantity } = props;
 
   return (
-    <div>
-      <Text
-        size="extrasmall"
-        className="text-semibold color-white bg-color-gray-dark radius-lg p_5"
-      >
-        {quantity}
-      </Text>
+    <div className="QuantitySpinner inline-flex justify-center items-center bg-color-gray-light py_5">
+      <Button className="QuantitySpinner__icon" alt="decrement">
+        <Icon className="flex" icon="Minus" fill="gray" />
+      </Button>
+      <span className="QuantitySpinner__count text-center bg-color-gray-dark radius-lg mx_5">
+        <Text size="extrasmall" className="text-semibold color-white">
+          {quantity}
+        </Text>
+      </span>
+      <Button className="QuantitySpinner__icon" alt="increment">
+        <Icon className="flex" icon="Plus" fill="gray" />
+      </Button>
     </div>
   );
 });
