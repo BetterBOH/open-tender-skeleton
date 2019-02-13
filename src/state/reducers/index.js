9d61a5dc141cb84aa4b027b8ff975b8e8b09a4a5
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-13 14:25:04 -0500

    remove unused duplicate orders reducer

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,5 @@
 import { reducer as openTender } from 'brandibble-redux';
 import status from './status';
-import order from './order';
 import locations from './locations';
 import geocoder from './geocoder';
 
