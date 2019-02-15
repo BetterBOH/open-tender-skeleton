6d63eb2a1778312d33e3acad7f2f8d35945191c4
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-15 17:39:08 -0500

    convert AccountButton + Anchor to memo

--- src/components/AccountButton/index.js
+++ src/components/AccountButton/index.js
@@ -1,14 +1,15 @@
+import React from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 
 import PropTypes from 'prop-types';
 import CustomerModel from 'constants/Models/CustomerModel';
 import withLocales from 'lib/withLocales';
 
-const AccountButton = props => {
+const AccountButton = React.memo(props => {
   return RegistryLoader(props, 'components.AccountButton', () =>
     import('./presentation.js')
   );
-};
+});
 
 AccountButton.propTypes = {
   className: PropTypes.string,
