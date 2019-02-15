1fb495c1e99def16ab7a7f4501b309b5c6ea9b81
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-15 17:56:36 -0500

    convert LocationCard and LocationInfoCard to memo

--- src/components/LocationCard/index.js
+++ src/components/LocationCard/index.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 
 import LocationModel from 'constants/Models/LocationModel';
 import withLocales from 'lib/withLocales';
 
-const LocationCard = props => {
-  return RegistryLoader(props, 'components.LocationCard', () =>
+const LocationCard = React.memo(props =>
+  RegistryLoader(props, 'components.LocationCard', () =>
     import('./presentation.js')
-  );
-};
+  )
+);
 
 LocationCard.propTypes = {
   location: LocationModel.propTypes
--- src/components/LocationInfoCard/index.js
+++ src/components/LocationInfoCard/index.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 
 import LocationModel from 'constants/Models/LocationModel';
 import withLocales from 'lib/withLocales';
 
-const LocationInfoCard = props => {
-  return RegistryLoader(props, 'components.LocationInfoCard', () =>
+const LocationInfoCard = React.memo(props =>
+  RegistryLoader(props, 'components.LocationInfoCard', () =>
     import('./presentation.js')
-  );
-};
+  )
+);
 
 LocationInfoCard.propTypes = {
   location: LocationModel.propTypes
