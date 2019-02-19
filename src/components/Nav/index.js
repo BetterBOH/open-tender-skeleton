7f6fb268d1a77637e6910f451a20eaa615da6a87
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-19 18:41:14 -0500

    cleanup Nav index

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 import PropTypes from 'prop-types';
-import CustomerModel from 'constants/Models/CustomerModel';
 
 const Nav = React.memo(props => {
-  // TODO: Replace mock customer data
   return RegistryLoader({ ...props }, 'components.Nav', () =>
     import('./presentation.js')
   );
