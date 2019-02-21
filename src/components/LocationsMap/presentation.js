a2c3f7c167bdb2f378cb1d5d3bc483c731889bd9
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-21 13:59:42 -0500

    add global variables to SCSS

--- src/components/LocationsMap/presentation.js
+++ src/components/LocationsMap/presentation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { MapboxMap } from 'components';
 
 const LocationsMap = React.memo(({ mapbox }) => (
-  <div className="col-12 md:col-7 lg:col-9 vh100 bg-color-white relative">
+  <div className="LocationsMap col-12 md:col-7 lg:col-9 bg-color-white relative">
     <MapboxMap {...mapbox} />
   </div>
 ));
