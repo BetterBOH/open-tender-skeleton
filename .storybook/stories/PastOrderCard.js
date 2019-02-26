7812e3ee7edc8879ea9a374bea87407e185705e6
Kay Mok <kaymok3@gmail.com>
2019-02-26 13:40:23 -0500

    Add PastOrderCard story

---
+++ .storybook/stories/PastOrderCard.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { checkA11y } from '@storybook/addon-a11y';
+
+import { order } from 'constants/Mocks';
+import { LocalesContext, localesRegistry } from '../mockConfig';
+import BrandStyle from 'lib/BrandStyle';
+import { brand } from '../brand';
+
+import { PastOrderCard } from 'components/PastOrderCard';
+import documentation from 'components/PastOrderCard/README.md';
+import 'styles.scss';
+
+const addons = {
+  notes: { markdown: documentation }
+};
+
+storiesOf('PastOrderCard', module)
+  .addDecorator(checkA11y)
+  .addDecorator(story => (
+    <React.Suspense fallback={<div />}>
+      <LocalesContext.Provider value={localesRegistry}>
+        {story()}
+      </LocalesContext.Provider>
+    </React.Suspense>
+  ))
+  .add(
+    'default',
+    () => (
+      <LocalesContext.Consumer>
+        {context => (
+          <div className="col-12 md:col-5 lg:col-4">
+            <BrandStyle brand={brand} />
+            <PastOrderCard order={order} localesContext={context} />
+          </div>
+        )}
+      </LocalesContext.Consumer>
+    ),
+    addons
+  );
