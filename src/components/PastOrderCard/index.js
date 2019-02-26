4a632e89a3d3dd5234c2bdb9a93e48ad4e6136f0
Kay Mok <kaymok3@gmail.com>
2019-02-26 13:25:08 -0500

    Set up PastOrderCard component

---
+++ src/components/PastOrderCard/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import RegistryLoader from 'lib/RegistryLoader';
+
+import OrderModel from 'constants/Models/OrderModel';
+import withLocales from 'lib/withLocales';
+
+const PastOrderCard = React.memo(props =>
+  RegistryLoader(props, 'components.PastOrderCard', () =>
+    import('./presentation.js')
+  )
+);
+
+PastOrderCard.propTypes = {
+  order: OrderModel.propTypes
+};
+
+PastOrderCard.defaultProps = {
+  order: OrderModel.defaultProps
+};
+
+export { PastOrderCard };
+export default withLocales(PastOrderCard);
---
+++ src/components/PastOrderCard/presentation.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import get from 'utils/get';
+
+import { Card, Text, Button, Icon } from 'components';
+
+const PastOrderCard = React.memo(props => {
+  const { order, localesContext } = props;
+  const { Language } = localesContext;
+
+  const locationName = get(order, 'location_name');
+  const requestedDate = get(order, 'requested_date');
+  const items = get(order, 'items');
+
+  return (
+    <Card className="PastOrderCard color-gray-dark uppercase p_5">
+      <Text className="bold" size="extrasmall">
+        {locationName}
+      </Text>
+      <Text className="bold color-black" size="small">
+        {requestedDate}
+      </Text>
+      {items.map(item => (
+        <Text size="small">{item.name}</Text>
+      ))}
+      <Button
+        variant="secondary"
+        onClick={f => f}
+        className="bg-color-gray-light flex items-center px1 py_5"
+      >
+        <div className="LocationInfoCard__button-icon mr_5">
+          <Icon fill={grayDark} icon="Repeat" />
+        </div>
+        <Text
+          size="extrasmall"
+          className="text-extrabold uppercase letter-spacing-sm"
+        >
+          {Language.t('order.reOrder')}
+        </Text>
+      </Button>
+      <Button variant="no-style">
+        <Text
+          size="extrasmall"
+          className="text-extrabold uppercase letter-spacing-sm"
+        >
+          {Language.t('order.details')}
+        </Text>
+      </Button>
+    </Card>
+  );
+});
+
+export default PastOrderCard;
