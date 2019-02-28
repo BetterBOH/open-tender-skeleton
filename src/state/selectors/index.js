ec96624d3240e2131d1ded776f7a4f95880574db
Joshie Fishbein <joshie@sanctuary.computer>
2019-02-28 15:49:37 -0500

    add MenuHero

--- src/state/selectors/index.js
+++ src/state/selectors/index.js
@@ -5,15 +5,23 @@ import accountDetails from './accountDetails';
 import filteredLocationsGeoJSON from './filteredLocationsGeoJSON';
 import locationIdFromMenuUrl from './locationIdFromMenuUrl';
 import currentLocation from './currentLocation';
+<<<<<<< HEAD
 import currentMenu from './currentMenu';
+=======
+>>>>>>> add MenuHero
 
 export {
   geocoderResultFeatures,
   userIsAuthenticated,
   parsedResetToken,
   filteredLocationsGeoJSON,
+<<<<<<< HEAD
   accountDetails,
   locationIdFromMenuUrl,
   currentLocation,
   currentMenu
+=======
+  locationIdFromMenuUrl,
+  currentLocation
+>>>>>>> add MenuHero
 };
