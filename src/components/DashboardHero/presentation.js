3111e89b3141926e51d7192b6379e12e12abcb4b
Kay Mok <kaymok3@gmail.com>
2019-03-07 17:47:05 -0500

    Add icon to dashboard hero

--- src/components/DashboardHero/presentation.js
+++ src/components/DashboardHero/presentation.js
@@ -9,6 +9,15 @@ const DashboardHero = React.memo(props => {
   return (
     <div className="vh100 relative">
       <HeroImage src={heroImageUrl} />
+      <div className="DashboardHero__icon-container absolute w100 flex justify-center">
+        <div className="bg-color-white shadow-md circle">
+          <Icon
+            className="DashboardHero__icon shadow-md"
+            icon="UserCircle"
+            fill="gray"
+          />
+        </div>
+      </div>
       <Card
         variant="dashboard-hero"
         className="col-12 bg-color-white text-center z1 py1_5"
