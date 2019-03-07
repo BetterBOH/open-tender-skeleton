e743771c83b0e58b44fe0caebdc329e8c135b2a7
Kay Mok <kaymok3@gmail.com>
2019-03-07 17:00:35 -0500

    Move Card prop types to index, add variant as prop to Card, and add README

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,4 +5,19 @@ const Card = React.memo(props =>
   RegistryLoader(props, 'components.Card', () => import('./presentation.js'))
 );
 
+Card.PropTypes = {
+  variant: PropTypes.string,
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ])
+};
+
+Card.defaultProps = {
+  variant: null,
+  className: '',
+  children: null
+};
+
 export default Card;
--- src/components/Card/presentation.js
+++ src/components/Card/presentation.js
@@ -2,28 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
 
-const Card = React.memo(({ className, children }) => (
-  <div
-    className={cx(
-      'Card flex flex-col flex-wrap bg-color-white-overlay shadow-md radius-md overflow-hidden',
-      className
-    )}
-  >
-    {children}
-  </div>
-));
+const Card = React.memo(({ variant, className, children }) => {
+  const classes = cx(
+    'Card flex flex-col flex-wrap overflow-hidden radius-md',
+    `Card--${variant}`,
+    className,
+    { 'bg-color-white-overlay shadow-md': !variant }
+  );
 
-Card.PropTypes = {
-  className: PropTypes.string,
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node
-  ])
-};
-
-Card.defaultProps = {
-  className: '',
-  children: null
-};
+  return <div className={classes}>{children}</div>;
+});
 
 export default Card;
