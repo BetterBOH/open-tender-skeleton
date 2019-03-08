d5f9e19e7ca634c7e26754c67efe0ccce1958dd2
Kay Mok <kaymok3@gmail.com>
2019-03-08 10:50:58 -0500

    Remove Language for now

--- src/components/DashboardHero/presentation.js
+++ src/components/DashboardHero/presentation.js
@@ -23,16 +23,11 @@ const DashboardHero = React.memo(props => {
         className="col-12 bg-color-white text-center z1 py1_5"
       >
         <Text size="headline" className="block my1">
-          {/* {!!customerFirstName ?
-            `${Language.t('dashboard.welcomeBack')}!` :
-            `${Language.t('dashboard.welcomeBack')}, ${customerFirstName}!`
-          } */}
           {!!customerFirstName
             ? `Welcome back, ${customerFirstName}!`
             : 'Welcome back!'}
         </Text>
         <Text size="description" className="block color-gray-dark">
-          {/* {Language.t('dashboard.adlib')} */}
           How can we serve you today?
         </Text>
       </Card>
