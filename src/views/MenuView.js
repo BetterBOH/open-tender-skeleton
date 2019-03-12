78711b573402519d3689e296bf755ae1a7ce57a4
Ellis Marte <elliscmarte@gmail.com>
2019-03-12 16:50:34 -0400

    sketch out submitting an order requirements

--- src/views/MenuView.js
+++ src/views/MenuView.js
@@ -8,10 +8,25 @@ import { MenuHero, Menus } from 'components';
 class MenuView extends PureComponent {
   render() {
     const { currentLocation, menu } = this.props;
-
     return (
       <main className="container relative">
         <MenuHero location={currentLocation} />
+        <div
+          style={{
+            backgroundColor: 'orange',
+            width: '200px',
+            borderRadius: '2px',
+            margin: '10px',
+            textAlign: 'center',
+            height: '24px',
+            pointer: 'cursor'
+          }}
+          onClick={() => {
+            this.props.history.push('/checkout');
+          }}
+        >
+          Checkout
+        </div>
         <Menus menu={menu} />
       </main>
     );
