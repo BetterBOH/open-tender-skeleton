e00ae10ebc012bd62b615e531a932a4211b60e14
Sebastian <sebastian@sanctuary.computer>
2019-03-14 17:41:34 -0400

    export date_time const

--- src/constants/DateTimeFormats.js
+++ src/constants/DateTimeFormats.js
@@ -1,3 +1,3 @@
 export const DATE_SHORT = 'L/d/y'; /* 8/2/1983 */
 export const DATE_FULL = 'LLLL d, y'; /* August 2, 1983 */
-const DATE_AND_TIME = 'M/d/yyyy h:mm a'; /* 8/6/2020 1:00 PM */
+export const DATE_AND_TIME = 'M/d/yyyy h:mm a'; /* 8/6/2020 1:00 PM */
