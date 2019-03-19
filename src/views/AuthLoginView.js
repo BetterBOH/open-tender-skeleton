7f8a065f36d333d77712aa7a47256b2a2a5be6ed
Kay Mok <kaymok3@gmail.com>
2019-03-19 18:41:19 -0400

    Add AuthView height

--- src/views/AuthLoginView.js
+++ src/views/AuthLoginView.js
@@ -17,8 +17,8 @@ class AuthLoginView extends PureComponent {
     if (userIsAuthenticated) return <Redirect to="/dashboard" />;
 
     return (
-      <main className="container relative">
-        <div className="col-12 md:col-5 lg:col-4 p1">
+      <main className="AuthView__container container relative flex items-center">
+        <div className="col-12 md:col-5 lg:col-4 md:ml4 p1">
           <AuthLogin
             attemptedEmail={attemptedEmail}
             actions={actions}
--- src/views/AuthView.js
+++ src/views/AuthView.js
@@ -12,8 +12,8 @@ class AuthView extends PureComponent {
     if (userIsAuthenticated) return <Redirect to="/dashboard" />;
 
     return (
-      <main className="container relative">
-        <div className="col-12 md:col-5 lg:col-4 p1">
+      <main className="AuthView__container container relative flex items-center">
+        <div className="col-12 md:col-5 lg:col-4 md:ml4 p1">
           <AuthEmailCheck actions={actions} openTenderRef={openTenderRef} />
         </div>
         <BackButton
