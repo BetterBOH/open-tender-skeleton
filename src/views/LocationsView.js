1c8b17462aa3c296d1287594cb72e40053b8fba4
Kay Mok <kaymok3@gmail.com>
2019-03-27 11:23:17 -0400

    Add DashboardOrderSummary to LocationsView and fix default props

--- src/views/LocationsView.js
+++ src/views/LocationsView.js
@@ -3,12 +3,17 @@ import React, { PureComponent } from 'react';
 import withComponents from 'lib/withComponents';
 import withLocales from 'lib/withLocales';
 
-import { LocationsMap, LocationsSearch } from 'components';
+import {
+  DashboardOrderSummary,
+  LocationsMap,
+  LocationsSearch
+} from 'components';
 
 class LocationsView extends PureComponent {
   render() {
     return (
       <main className="container relative">
+        <DashboardOrderSummary />
         <div className="flex flex-wrap flex-row-reverse">
           <LocationsMap
             featureCollection={this.props.filteredLocationsGeoJSON}
