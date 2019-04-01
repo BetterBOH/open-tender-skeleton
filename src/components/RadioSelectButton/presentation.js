66bd0473e8a30b818fd01a5b7621ec8ae5d6455f
Kay Mok <kaymok3@gmail.com>
2019-04-01 18:03:41 -0400

    Refactor

--- src/components/RadioSelectButton/presentation.js
+++ src/components/RadioSelectButton/presentation.js
@@ -4,30 +4,34 @@ import cx from 'classnames';
 import { Button, RadioInput } from 'components';
 
 const RadioSelectButton = React.memo(
-  ({ className, id, variant, name, children, isSelected, onClick }) => (
-    <Button
-      onClick={onClick}
-      variant="no-style"
-      className={cx(
-        'RadioSelectButton col-12 flex justify-between items-center py1',
-        {
-          'RadioSelectButton--standalone shadow-sm radius-md px1':
-            variant === 'standalone',
-          'border-color-gray-dark': variant === 'standalone' && isSelected,
-          'RadioSelectButton--list': variant === 'list'
-        },
-        className
-      )}
-    >
-      {children}
-      <RadioInput
-        className={cx({ mr2: variant === 'list' })}
-        id={id}
-        name={name}
-        checked={isSelected}
-      />
-    </Button>
-  )
+  ({ className, id, variant, name, children, isSelected, onClick }) => {
+    const isStandAlone = variant === 'standalone';
+    const isList = variant === 'list';
+
+    return (
+      <Button
+        onClick={onClick}
+        variant="no-style"
+        className={cx(
+          'RadioSelectButton col-12 flex justify-between items-center py1',
+          {
+            'RadioSelectButton--standalone shadow-sm radius-md px1': isStandAlone,
+            'border-color-gray-dark': isStandAlone && isSelected,
+            'RadioSelectButton--list': isList
+          },
+          className
+        )}
+      >
+        {children}
+        <RadioInput
+          className={cx({ mr2: isList })}
+          id={id}
+          name={name}
+          checked={isSelected}
+        />
+      </Button>
+    );
+  }
 );
 
 export default RadioSelectButton;
