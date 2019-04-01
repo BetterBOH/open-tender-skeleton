b3bea63b971495a7d0d778bda9cf17583895d47a
Joshie Fishbein <joshie@sanctuary.computer>
2019-04-01 17:02:39 -0400

    apply getters to OptionGroup

--- src/components/OptionGroup/presentation.js
+++ src/components/OptionGroup/presentation.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Text, Card, OptionGroupItem } from 'components';
+import get from 'utils/get';
 
 const OptionGroup = React.memo(({ optionGroup, lineItem }) => (
   <div className="LineItemEditor__option-group my2 px2">
     <Text size="body" className="block text-semibold mb_5">
-      {optionGroup.name}
+      {get(optionGroup, 'name', '')}
     </Text>
     <Text size="small" className="block text-semibold color-gray mb1">
-      {optionGroup.description}
+      {get(optionGroup, 'description', '')}
     </Text>
     <Card variant="small" className="px2 py1 shadow-sm bg-color-white">
-      {optionGroup.option_items.map(optionItem => (
+      {get(optionGroup, 'option_items', []).map(optionItem => (
         <OptionGroupItem
           item={optionItem}
           optionGroup={optionGroup}
