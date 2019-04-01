5035f016646b8d2aa690a5a705eab3442bdce157
Sebastian <sebastian@sanctuary.computer>
2019-04-01 17:28:43 -0400

    adds subtotal row

--- src/components/MiniCart/presentation.js
+++ src/components/MiniCart/presentation.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Text, Button, Icon } from 'components';
+import { Text, Button, Icon, OrderSubtotal } from 'components';
 
 const MiniCart = React.memo(
   ({ handleAddMore, handleCheckOut, handleClose }) => {
     return (
-      <div className="MiniCart h100 w100 bg-color-black">
-        <div className="MiniCart__footer fixed r0 b0 l0 p1_5 bg-color-white">
-          <div className="MiniCart__footer__actions-row flex">
+      <div className="MiniCart h100 w100 bg-color-gray-light">
+        <div className="MiniCart__footer fixed r0 b0 l0 bg-color-white">
+          <div className="MiniCart__footer__subtotal-row">
+            <OrderSubtotal subtotal="0.00" />
+          </div>
+          <div className="MiniCart__footer__actions-row bg-color-gray-light p1_5 flex">
             <Button
               className={`flex-1 mr1 bg-color-white color-black shadow-md`}
               variant="primary"
