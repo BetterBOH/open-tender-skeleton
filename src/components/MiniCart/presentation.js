b9c902ba54af2fa68a7c3335abde3121b9b2cc14
Sebastian <sebastian@sanctuary.computer>
2019-04-01 17:54:44 -0400

    adds remaining child components (with mocks)

--- src/components/MiniCart/presentation.js
+++ src/components/MiniCart/presentation.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Text, Button, Icon, OrderSubtotal } from 'components';
+import {
+  Text,
+  Button,
+  Icon,
+  OrderSubtotal,
+  OrderSummary,
+  LineItemsCard
+} from 'components';
+
+/* TODO: Use real data */
+import { customer, lineItemsData } from 'constants/Mocks';
 
 const MiniCart = React.memo(
   ({ handleAddMore, handleCheckOut, handleClose, localesContext }) => {
@@ -7,6 +17,27 @@ const MiniCart = React.memo(
 
     return (
       <div className="MiniCart h100 w100 bg-color-gray-light">
+        <div className="MiniCart__order-summary-container p1_5">
+          <OrderSummary
+            orderSummaryData={{
+              serviceType: 'Pickup',
+              orderTime: '4:20pm',
+              locationName: 'Fleeker St.',
+              locationImage: null,
+              customer: customer
+            }}
+          />
+        </div>
+        <div className="MiniCart__line-items-card-container p1_5">
+          <LineItemsCard
+            items={lineItemsData}
+            handleDecrement={f => f}
+            handleIncrement={f => f}
+            isConfigurable={true}
+            showItemsWithoutQuantity={false}
+            customer={customer}
+          />
+        </div>
         <div className="MiniCart__footer fixed r0 b0 l0 bg-color-white">
           <div className="MiniCart__footer__subtotal-row">
             <OrderSubtotal subtotal="0.00" />
