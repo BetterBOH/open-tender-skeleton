ccd3678b0a4e522a08213babeee2c972b4bcd906
Kay Mok <kaymok3@gmail.com>
2019-04-03 14:19:46 -0400

    Add geolocateUser function to get coordinates

--- src/components/LocateMeButton/index.js
+++ src/components/LocateMeButton/index.js
@@ -13,15 +13,31 @@ class LocateMeButton extends PureComponent {
     className: ''
   };
 
-  handleClick = () => {
-    console.log('clicked');
+  geolocateUser = () => {
+    const success = position => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+
+      console.log(`Your location is ${latitude} and ${longitude}`);
+    };
+
+    const error = () => {
+      console.log('Unable to retrieve your location');
+    };
+
+    if (!navigator.geolocation) {
+      console.log('Geolocation is not supported by your browser');
+    } else {
+      console.log('Locating...');
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
   };
 
   render() {
     return RegistryLoader(
       {
         className: this.props.className,
-        handleClick: this.handleClick,
+        handleClick: this.geolocateUser,
         localesContext: this.props.localesContext
       },
       'components.LocateMeButton',
