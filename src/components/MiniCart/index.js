81c031fa5d199fd8c261dcf897b0c093be944e79
Sebastian <sebastian@sanctuary.computer>
2019-04-03 11:25:53 -0400

    adds proptypes

--- src/components/MiniCart/index.js
+++ src/components/MiniCart/index.js
@@ -1,7 +1,11 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import { withRouter, matchPath } from 'react-router-dom';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import CustomerModel from 'constants/Models/CustomerModel';
+import OrderModel from 'constants/Models/OrderModel';
+import LineItemModel from 'constants/Models/LineItemModel';
+import LocationModel from 'constants/Models/LocationModel';
 import RegistryLoader from 'lib/RegistryLoader';
 import withLocales from 'lib/withLocales';
 import withRoutes from 'lib/withRoutes';
@@ -15,11 +19,24 @@ import {
 
 class MiniCart extends PureComponent {
   static propTypes = {
-    handleClose: PropTypes.func
+    handleClose: PropTypes.func,
+    currentOrder: OrderModel.propTypes,
+    currentCustomer: CustomerModel.propTypes,
+    lineItemsData: PropTypes.arrayOf(LineItemModel),
+    currentLocation: LocationModel.propTypes,
+    subtotal: PropTypes.string,
+    userIsAuthenticated: PropTypes.bool
   };
 
   static defaultProps = {
-    handleClose: f => f
+    handleClose: f => f,
+    currentCustomer: CustomerModel.defaultProps,
+    currentOrder: OrderModel.defaultProps,
+    currentCustomer: CustomerModel.defaultProps,
+    lineItemsData: [],
+    currentLocation: LocationModel.defaultProps,
+    subtotal: '0.00',
+    userIsAuthenticated: false
   };
 
   goToCurrentMenuPath = () => {
