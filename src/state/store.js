1190f9ba8ddd3331222c6ec16f233892bb236e24
Joshie Fishbein <joshie@sanctuary.computer>
2019-04-04 14:58:34 -0400

    update storybook, adjust history import in store to fix problem in storybook boot

--- src/state/store.js
+++ src/state/store.js
@@ -2,13 +2,13 @@ import { combineReducers, createStore, applyMiddleware, compose } from 'redux';
 import { brandibbleMiddleware as openTenderMiddleware } from 'brandibble-redux';
 
 import { connectRouter, routerMiddleware } from 'connected-react-router';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import createSagaMiddleware from 'redux-saga';
 
 import reducers from 'state/reducers';
 import sagas from 'state/sagas';
 
-const history = createHistory();
+const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
 
 const middleware = [
