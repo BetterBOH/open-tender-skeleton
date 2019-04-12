a0c16c283a25fe9008c6bfa809ee1d432e424df7
Kay Mok <kaymok3@gmail.com>
2019-04-12 13:12:23 -0400

    Add debounce and use to get menuNav scroll position

--- src/components/MenuNav/presentation.js
+++ src/components/MenuNav/presentation.js
@@ -1,46 +1,81 @@
-import React from 'react';
+import React, { PureComponent, createRef } from 'react';
 import cx from 'classnames';
 import { Button, Text, Icon } from 'components';
+import debounce from 'utils/debounce';
 
-const MenuNav = React.memo(props => {
-  const {
-    menuTitle,
-    selectedCategory,
-    menuNavModalIsActive,
-    handleClick
-  } = props;
+class MenuNav extends PureComponent {
+  constructor() {
+    super(...arguments);
 
-  return (
-    <nav
-      className={cx(
-        'MenuNav relative col-12 bg-color-white flex justify-between items-center p1',
-        {
-          'fixed t0 l0 r0 z3': menuNavModalIsActive
-        }
-      )}
-    >
-      <Button
-        className="MenuNav__button flex items-center"
-        onClick={handleClick}
-      >
-        <Text
-          size="description"
-          className={
-            menuNavModalIsActive ? 'text-bold color-black' : 'color-gray'
+    this.menuNavRef = createRef();
+
+    this.state = {
+      scrolledOutOfView: false
+    };
+  }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', debounce(this.handleScroll, 16));
+  };
+
+  componentWillUnmount = () => {
+    window.removeEventListener('scroll', debounce(this.handleScroll, 16));
+  };
+
+  handleScroll = () => {
+    const scrollPositionY = +window.scrollY;
+    const menuNav = this.menuNavRef.current;
+    const menuNavHeight = menuNav.clientHeight;
+    const menuNavDistanceFromViewportTop = menuNav.getBoundingClientRect().top;
+    const scrolledOutOfView =
+      scrollPositionY > menuNavDistanceFromViewportTop + menuNavHeight;
+
+    if (this.state.scrolledOutOfView !== scrolledOutOfView) {
+      this.setState({ scrolledOutOfView });
+    }
+  };
+
+  render() {
+    const {
+      menuTitle,
+      selectedCategory,
+      menuNavModalIsActive,
+      handleClick
+    } = this.props;
+
+    return (
+      <nav
+        className={cx(
+          'MenuNav relative col-12 bg-color-white flex justify-between items-center p1',
+          {
+            'fixed t0 l0 r0 z3': this.state.scrolledOutOfView
           }
-          aria-label={selectedCategory || menuTitle}
+        )}
+        ref={this.menuNavRef}
+      >
+        <Button
+          className="MenuNav__button flex items-center"
+          onClick={handleClick}
         >
-          {selectedCategory || menuTitle}
-        </Text>
-        <div className="MenuNav__icon ml_5">
-          <Icon
-            icon={menuNavModalIsActive ? 'Dropup' : 'Dropdown'}
-            fill="gray"
-          />
-        </div>
-      </Button>
-    </nav>
-  );
-});
+          <Text
+            size="description"
+            className={
+              menuNavModalIsActive ? 'text-bold color-black' : 'color-gray'
+            }
+            aria-label={selectedCategory || menuTitle}
+          >
+            {selectedCategory || menuTitle}
+          </Text>
+          <div className="MenuNav__icon ml_5">
+            <Icon
+              icon={menuNavModalIsActive ? 'Dropup' : 'Dropdown'}
+              fill="gray"
+            />
+          </div>
+        </Button>
+      </nav>
+    );
+  }
+}
 
 export default MenuNav;
