66fdb321f7bc6d83a69cda03b88fda2b67139227
Sebastian <sebastian@sanctuary.computer>
2019-04-12 10:14:27 -0400

    adds back buttons functionality to OrderSummary

--- src/components/OrderSummaryButtons/index.js
+++ src/components/OrderSummaryButtons/index.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RegistryLoader from 'lib/RegistryLoader';
+import { getConfig } from 'lib/MutableConfig';
+import ConfigKeys from 'constants/ConfigKeys';
+import get from 'utils/get';
 
-const OrderSummaryButtons = React.memo(({ localesContext, orderIsPending }) =>
-  RegistryLoader(
-    { localesContext, orderIsPending },
-    'components.OrderSummaryButtons',
-    () => import('./presentation.js')
-  )
+const OrderSummaryButtons = React.memo(
+  ({ localesContext, orderIsPending, userIsAuthenticated, push }) => {
+    const handleGoBack = () => {
+      const dashboardPath = get(getConfig(ConfigKeys.ROUTES), 'dashboard.path');
+
+      if (userIsAuthenticated) {
+        return push(dashboardPath);
+      }
+
+      return push('/');
+    };
+
+    return RegistryLoader(
+      { localesContext, orderIsPending, handleGoBack },
+      'components.OrderSummaryButtons',
+      () => import('./presentation.js')
+    );
+  }
 );
 
 OrderSummaryButtons.propTypes = {
-  orderIsPending: PropTypes.bool
+  orderIsPending: PropTypes.bool,
+  push: PropTypes.func,
+  userIsAuthenticated: PropTypes.bool
 };
 
 OrderSummaryButtons.defaultProps = {
-  orderIsPending: true
+  orderIsPending: true,
+  push: f => f,
+  userIsAuthenticated: false
 };
 
 export default OrderSummaryButtons;
--- src/components/OrderSummaryButtons/presentation.js
+++ src/components/OrderSummaryButtons/presentation.js
@@ -2,53 +2,55 @@ import React from 'react';
 
 import { Button, Icon, Text } from 'components';
 
-const OrderSummaryButtons = React.memo(({ localesContext, orderIsPending }) => {
-  const { Language } = localesContext;
-  return (
-    <div className="CheckoutButtons flex">
-      <Button
-        variant="icon-circle-primary"
-        className="bg-color-gray-dark"
-        onClick={f => f}
-      >
-        <Icon fill="white" icon="Back" />
-      </Button>
-      {!orderIsPending && (
+const OrderSummaryButtons = React.memo(
+  ({ localesContext, orderIsPending, handleGoBack }) => {
+    const { Language } = localesContext;
+    return (
+      <div className="CheckoutButtons flex">
         <Button
-          variant="primary"
-          onClick={f => f}
-          className="bg-color-white flex flex-1 justify-center items-center ml1 px1 py_5"
+          variant="icon-circle-primary"
+          className="bg-color-gray-dark"
+          onClick={handleGoBack}
         >
-          <div className="LocationCard__order-button-icon mr_5">
-            <Icon icon="Bubbles" />
-          </div>
-          <Text
-            size="extrasmall"
-            className="text-extrabold uppercase letter-spacing-md"
-          >
-            {Language.t('orderSummary.feedback')}
-          </Text>
+          <Icon fill="white" icon="Back" />
         </Button>
-      )}
-      {!orderIsPending && (
-        <Button
-          variant="primary"
-          onClick={f => f}
-          className="bg-color-black flex flex-1 justify-center items-center ml1 px1 py_5"
-        >
-          <div className="LocationCard__order-button-icon mr_5">
-            <Icon fill="white" icon="Repeat" />
-          </div>
-          <Text
-            size="extrasmall"
-            className="text-extrabold color-white uppercase letter-spacing-md"
+        {!orderIsPending && (
+          <Button
+            variant="primary"
+            onClick={f => f}
+            className="bg-color-white flex flex-1 justify-center items-center ml1 px1 py_5"
           >
-            {Language.t('orderSummary.reorder')}
-          </Text>
-        </Button>
-      )}
-    </div>
-  );
-});
+            <div className="LocationCard__order-button-icon mr_5">
+              <Icon icon="Bubbles" />
+            </div>
+            <Text
+              size="extrasmall"
+              className="text-extrabold uppercase letter-spacing-md"
+            >
+              {Language.t('orderSummary.feedback')}
+            </Text>
+          </Button>
+        )}
+        {!orderIsPending && (
+          <Button
+            variant="primary"
+            onClick={f => f}
+            className="bg-color-black flex flex-1 justify-center items-center ml1 px1 py_5"
+          >
+            <div className="LocationCard__order-button-icon mr_5">
+              <Icon fill="white" icon="Repeat" />
+            </div>
+            <Text
+              size="extrasmall"
+              className="text-extrabold color-white uppercase letter-spacing-md"
+            >
+              {Language.t('orderSummary.reorder')}
+            </Text>
+          </Button>
+        )}
+      </div>
+    );
+  }
+);
 
 export default OrderSummaryButtons;
