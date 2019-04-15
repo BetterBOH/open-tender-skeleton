e4d91226bfb34dfcf0fa16509db4ebb7fbae3e7f
Kay Mok <kaymok3@gmail.com>
2019-04-15 15:09:08 -0400

    Change to static props

--- src/components/ScrollTo/ScrollToSection.js
+++ src/components/ScrollTo/ScrollToSection.js
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types';
 import { Element } from 'react-scroll';
 
 class ScrollToSection extends PureComponent {
+  static propTypes = {
+    sectionName: PropTypes.string,
+    children: PropTypes.node.isRequired
+  };
+
+  static defaultProps = {
+    sectionName: ''
+  };
+
   render() {
     const { children, sectionName } = this.props;
     return <Element name={sectionName}>{children}</Element>;
   }
 }
-
-ScrollToSection.defaultProps = {
-  sectionName: ''
-};
-
-ScrollToSection.propTypes = {
-  sectionName: PropTypes.string,
-  children: PropTypes.node.isRequired
-};
 
 export default ScrollToSection;
