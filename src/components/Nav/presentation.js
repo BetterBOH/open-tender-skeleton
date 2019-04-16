f989891c6c0884b01c9dfedafa00a5b29275b1da
Joshie Fishbein <joshie@sanctuary.computer>
2019-04-16 12:19:56 -0400

    add link to welcome page to Nav

--- src/components/Nav/presentation.js
+++ src/components/Nav/presentation.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import get from 'utils/get';
+import ConfigKeys from 'constants/ConfigKeys';
+import { getConfig } from 'lib/MutableConfig';
+import { Link } from 'react-router-dom';
 
 import { Image, AccountButton } from 'components';
 
-const Nav = React.memo(({ brandContext, customer }) => {
-  const logo = get(brandContext, 'logoImage');
+const welcomePath = get(getConfig(ConfigKeys.ROUTES), 'welcome.path');
 
-  return (
-    <div className="Nav relative p1 bg-color-white flex justify-between items-center border-color-gray-light">
-      <Image className="h100" src={logo} />
-      <AccountButton customer={customer} />
-    </div>
-  );
-});
+const Nav = React.memo(({ brandContext, customer }) => (
+  <div className="Nav relative px1 py_5 bg-color-white flex justify-between items-center border-color-gray-light">
+    <Link to={welcomePath} className="h100 py_25">
+      <Image className="h100" src={get(brandContext, 'logoImage')} />
+    </Link>
+    <AccountButton customer={customer} />
+  </div>
+));
 
 export default Nav;
