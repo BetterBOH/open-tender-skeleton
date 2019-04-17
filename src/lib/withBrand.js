a0ed93ed71f1b7b9b4055da71abe0193cc41cf17
Kay Mok <kaymok3@gmail.com>
2019-04-17 16:40:42 -0400

    Refactor

--- src/lib/withBrand.js
+++ src/lib/withBrand.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BrandContext } from 'config';
 import { BrandContext as MockContext } from 'tests/mocks/config';
 import environmentIsMock from 'utils/environmentIsMock';
 
-const withBrand = Component => {
-  class ComponentWithBrand extends React.Component {
+const withBrand = WrappedComponent => {
+  class ComponentWithBrandContext extends Component {
     static propTypes = {
-      ...Component.propTypes,
-      ...withBrand.propTypes
+      brandContext: PropTypes.shape({
+        brand: PropTypes.shape({
+          backgroundImage: PropTypes.string,
+          logoImage: PropTypes.string,
+          links: PropTypes.arrayOf(
+            PropTypes.shape({
+              name: PropTypes.string,
+              url: PropTypes.string
+            })
+          ),
+          theme: PropTypes.string,
+          colors: PropTypes.object
+        }),
+        registry: PropTypes.shape({
+          components: PropTypes.object
+        }),
+        locales: PropTypes.object,
+        mapbox: PropTypes.object
+      }),
+      ...WrappedComponent.propTypes
     };
 
     static defaultProps = {
-      ...Component.defaultProps,
-      ...withBrand.defaultProps
+      brandContext: {
+        brand: {
+          backgroundImage: '',
+          logoImage: '',
+          links: [],
+          theme: '',
+          colors: {}
+        },
+        registry: {
+          components: {}
+        },
+        locales: {},
+        mapbox: {}
+      },
+      ...WrappedComponent.defaultProps
     };
 
     render() {
-      return <Component {...this.props} />;
+      return <WrappedComponent {...this.props} />;
     }
   }
 
@@ -25,44 +56,11 @@ const withBrand = Component => {
 
   return React.memo(props => (
     <Context.Consumer>
-      {context => <ComponentWithBrand {...props} brandContext={context} />}
+      {context => (
+        <ComponentWithBrandContext {...props} brandContext={context} />
+      )}
     </Context.Consumer>
   ));
-};
-
-withBrand.propTypes = {
-  brand: PropTypes.shape({
-    backgroundImage: PropTypes.string,
-    logoImage: PropTypes.string,
-    links: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string,
-        url: PropTypes.string
-      })
-    ),
-    theme: PropTypes.string,
-    colors: PropTypes.object
-  }),
-  registry: PropTypes.shape({
-    components: PropTypes.object
-  }),
-  locales: PropTypes.object,
-  mapbox: PropTypes.object
-};
-
-withBrand.defaultProps = {
-  brand: {
-    backgroundImage: '',
-    logoImage: '',
-    links: [],
-    theme: '',
-    colors: {}
-  },
-  registry: {
-    components: {}
-  },
-  locales: {},
-  mapbox: {}
 };
 
 export default withBrand;
