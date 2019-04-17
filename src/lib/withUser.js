3bf06125359e83e7fdcdd70612f01800742c36ee
Kay Mok <kaymok3@gmail.com>
2019-04-17 18:37:41 -0400

    Add prop types to withUser

--- src/lib/withUser.js
+++ src/lib/withUser.js
@@ -1,10 +1,21 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { userIsAuthenticated } from 'state/selectors';
 
 const withUser = WrappedComponent => {
   class WithUser extends PureComponent {
+    static propTypes = {
+      userIsAuthenticated: PropTypes.bool,
+      ...WrappedComponent.propTypes
+    };
+
+    static defaultProps = {
+      userIsAuthenticated: false,
+      ...WrappedComponent.defaultProps
+    };
+
     render() {
       return <WrappedComponent {...this.props} />;
     }
