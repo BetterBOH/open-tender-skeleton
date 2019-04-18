49e55728de9ba3307b9e22fb107bd1a8d07f9bfa
Kay Mok <kaymok3@gmail.com>
2019-04-18 15:44:58 -0400

    Use DetailsCard in PastOrderDetails

--- src/components/PastOrderDetails/index.js
+++ src/components/PastOrderDetails/index.js
@@ -1,20 +1,74 @@
-import React from 'react';
+import { PureComponent } from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
-
+import get from 'utils/get';
 import OrderModel from 'constants/Models/OrderModel';
+import withLocales from 'lib/withLocales';
 
-const PastOrderDetails = React.memo(props =>
-  RegistryLoader(props, 'components.PastOrderDetails', () =>
-    import('./presentation.js')
-  )
-);
+class OrderDetails extends PureComponent {
+  static propTypes = {
+    order: OrderModel.propTypes
+  };
 
-PastOrderDetails.propTypes = {
-  order: OrderModel.propTypes
-};
+  static defaultProps = {
+    order: OrderModel.defaultProps
+  };
 
-PastOrderDetails.defaultProps = {
-  order: OrderModel.defaultProps
-};
+  render() {
+    const { order, localesContext } = this.props;
 
-export default PastOrderDetails;
+    const requestedDate = get(order, 'requested_date', '');
+    const requestedTime = get(order, 'requested_time', '');
+    const requestedDateAndTime =
+      !!requestedDate && !!requestedTime
+        ? `${requestedDate} at ${requestedTime}`
+        : '';
+
+    const cardType = get(order, 'credit_card.card_type', '');
+    const last4 = get(order, 'credit_card.last4', '');
+    const cardUsed =
+      !!cardType && !!last4
+        ? `${cardType} ${localesContext.Language.t('order.ccEndingIn')}${last4}`
+        : '';
+
+    const formattedOrderDetails = [
+      {
+        label: localesContext.Language.t('order.service'),
+        icon: 'Bag',
+        value: get(order, 'service_type_str', ''),
+        children: null
+      },
+      {
+        label: localesContext.Language.t('order.location'),
+        icon: 'Marker',
+        value: get(order, 'location_name', ''),
+        children: null
+      },
+      {
+        label: localesContext.Language.t('order.pickupTime'),
+        icon: 'Clock',
+        value: requestedDateAndTime,
+        children: null
+      },
+      {
+        label: localesContext.Language.t('order.contact'),
+        icon: 'Phone',
+        value: get(order, 'phone', ''),
+        children: null
+      },
+      {
+        label: localesContext.Language.t('order.payment'),
+        icon: 'CreditCard',
+        value: cardUsed,
+        children: null
+      }
+    ];
+
+    return RegistryLoader(
+      { formattedOrderDetails },
+      'components.OrderDetails',
+      () => import('./presentation.js')
+    );
+  }
+}
+
+export default withLocales(OrderDetails);
--- src/components/PastOrderDetails/presentation.js
+++ src/components/PastOrderDetails/presentation.js
@@ -1,73 +1,19 @@
 import React, { Fragment } from 'react';
-import get from 'utils/get';
+import { Text, DetailsCard } from 'components';
 
-import { Card, Text, DetailItemRow } from 'components';
-
-const PastOrderDetails = React.memo(props => {
-  const { order, localesContext } = props;
-  const { Language } = localesContext;
-
-  const serviceType = get(order, 'service_type_str', '');
-
-  const locationName = get(order, 'location_name', '');
-
-  const requestedDate = get(order, 'requested_date', '');
-  const requestedTime = get(order, 'requested_time', '');
-  const requestedDateAndTime =
-    !!requestedDate && !!requestedTime
-      ? `${requestedDate} at ${requestedTime}`
-      : '';
-
-  const phoneNumber = get(order, 'phone', '');
-
-  const cardType = get(order, 'credit_card.card_type', '');
-  const last4 = get(order, 'credit_card.last4', '');
-  const cardUsed =
-    !!cardType && !!last4
-      ? `${cardType} ${Language.t('order.ccEndingIn')}${last4}`
-      : '';
-
-  return (
-    <Fragment>
-      <div className="mb1">
-        <Text size="cta" className="bold">
-          {Language.t('order.details')}
-        </Text>
-      </div>
-      <Card className="PastOrderDetails px1_5 py_5">
-        <DetailItemRow
-          label={Language.t('order.service')}
-          icon="Bag"
-          value={serviceType}
-        />
-        <DetailItemRow
-          label={Language.t('order.location')}
-          icon="Marker"
-          value={locationName}
-        />
-        <DetailItemRow
-          label={Language.t('order.pickupTime')}
-          icon="Clock"
-          value={requestedDateAndTime}
-        />
-        <DetailItemRow
-          label={Language.t('order.pickupBy')}
-          icon="User"
-          value="Hugh"
-        />
-        <DetailItemRow
-          label={Language.t('order.contact')}
-          icon="Phone"
-          value={phoneNumber}
-        />
-        <DetailItemRow
-          label={Language.t('order.payment')}
-          icon="CreditCard"
-          value={cardUsed}
-        />
-      </Card>
-    </Fragment>
-  );
-});
+const PastOrderDetails = React.memo(
+  ({ formattedOrderDetails, localesContext }) => {
+    return (
+      <Fragment>
+        <div className="px1 mb_5">
+          <Text size="cta" className="bold">
+            {localesContext.Language.t('order.details')}
+          </Text>
+        </div>
+        <DetailsCard details={formattedOrderDetails} />
+      </Fragment>
+    );
+  }
+);
 
 export default PastOrderDetails;
