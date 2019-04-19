87fa759cd1eaa42031a56b6e14266af7b870cbde
Kay Mok <kaymok3@gmail.com>
2019-04-19 17:04:06 -0400

    Move formatted account details object to presentation

--- src/components/AccountDetails/index.js
+++ src/components/AccountDetails/index.js
@@ -1,8 +1,6 @@
 import { PureComponent } from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
-import get from 'utils/get';
 import AccountDetailsModel from 'constants/Models/AccountDetailsModel';
-import withLocales from 'lib/withLocales';
 
 class AccountDetails extends PureComponent {
   static propTypes = {
@@ -14,66 +12,12 @@ class AccountDetails extends PureComponent {
   };
 
   render() {
-    const { accountDetails, localesContext } = this.props;
+    const { accountDetails } = this.props;
 
-    const addressText = get(accountDetails, 'defaultAddress.street_address')
-      ? get(accountDetails, 'defaultAddress.street_address')
-      : localesContext.Language.t('account.addAddress');
-
-    const paymentText =
-      get(accountDetails, 'defaultPayment.card_type') &&
-      get(accountDetails, 'defaultPayment.last4')
-        ? `${get(
-            accountDetails,
-            'defaultPayment.card_type'
-          )} ${localesContext.Language.t('account.ccEndingIn')}${get(
-            accountDetails,
-            'defaultPayment.last4'
-          )}`
-        : localesContext.Language.t('account.noDefaultPayment');
-
-    const numberOfAddresses = `${localesContext.Language.t(
-      'account.delivery'
-    )} (${get(accountDetails, 'addresses.length', 0)})`;
-
-    const numberOfPayments = `${localesContext.Language.t(
-      'account.payment'
-    )} (${get(accountDetails, 'payments.length', 0)})`;
-
-    const formattedAccountDetails = [
-      {
-        label: localesContext.Language.t('account.name'),
-        icon: 'User',
-        value: get(accountDetails, 'fullName', '')
-      },
-      {
-        label: localesContext.Language.t('account.email'),
-        icon: 'At',
-        value: get(accountDetails, 'email', '')
-      },
-      {
-        label: localesContext.Language.t('account.password'),
-        icon: 'Lock',
-        value: '*********'
-      },
-      {
-        label: numberOfAddresses,
-        icon: 'Map',
-        value: addressText
-      },
-      {
-        label: numberOfPayments,
-        icon: 'CreditCard',
-        value: paymentText
-      }
-    ];
-
-    return RegistryLoader(
-      { formattedAccountDetails },
-      'components.AccountDetails',
-      () => import('./presentation.js')
+    return RegistryLoader({ accountDetails }, 'components.AccountDetails', () =>
+      import('./presentation.js')
     );
   }
 }
 
-export default withLocales(AccountDetails);
+export default AccountDetails;
