82f718afc258927b2598954af7ed6c16f7d05b6a
Joshie Fishbein <joshie@sanctuary.computer>
2019-04-22 11:20:12 -0400

    remove pickup/location view components that only apply to one view

--- src/views/LocationsView.js
+++ src/views/LocationsView.js
@@ -20,29 +20,17 @@ class LocationsView extends PureComponent {
 
     return (
       <main className="LocationsView__container container relative flex flex-col justify-center items-center md:flex-row md:justify-start">
-        {!!geolocations && geolocations.length ? (
-          <React.Fragment>
-            <LocationsMap
-              geolocations={geolocations}
-              featureCollection={filteredLocationsGeoJSON}
-              userCoordinates={userCoordinates}
-            />
-            <div className="LocationsSearch h100 overflow-y-scroll col-12 md:col-5 lg:col-3 bg-color-gray-light">
-              <div className="px1 py1_5 shadow-sm z-1">
-                <MapboxGeocoder />
-              </div>
-              <LocationsSearchResults />
-            </div>
-          </React.Fragment>
-        ) : (
-          <div className="col-12 md:col-6 lg:col-4 md:ml4 p1">
-            <div className="relative overflow-auto my2">
-              <LocationsSuggestionsCard
-                serviceType={get(orderRef, 'serviceType', PICKUP)}
-              />
-            </div>
+        <LocationsMap
+          geolocations={geolocations}
+          featureCollection={filteredLocationsGeoJSON}
+          userCoordinates={userCoordinates}
+        />
+        <div className="LocationsSearch h100 overflow-y-scroll col-12 md:col-5 lg:col-3 bg-color-gray-light">
+          <div className="px1 py1_5 shadow-sm z-1">
+            <MapboxGeocoder />
           </div>
-        )}
+          <LocationsSearchResults />
+        </div>
       </main>
     );
   }
--- src/views/PickupView.js
+++ src/views/PickupView.js
@@ -20,29 +20,13 @@ class PickupView extends PureComponent {
 
     return (
       <main className="PickupView__container container relative flex flex-col justify-center items-center md:flex-row md:justify-start">
-        {!!geolocations && geolocations.length ? (
-          <React.Fragment>
-            <LocationsMap
-              geolocations={geolocations}
-              featureCollection={filteredLocationsGeoJSON}
-              userCoordinates={userCoordinates}
+        <div className="col-12 md:col-6 lg:col-4 md:ml4 p1">
+          <div className="relative overflow-auto my2">
+            <LocationsSuggestionsCard
+              serviceType={get(orderRef, 'serviceType', PICKUP)}
             />
-            <div className="LocationsSearch h100 overflow-y-scroll col-12 md:col-5 lg:col-3 bg-color-gray-light">
-              <div className="px1 py1_5 shadow-sm z-1">
-                <MapboxGeocoder askForBrowserLocation={true} />
-              </div>
-              <LocationsSearchResults />
-            </div>
-          </React.Fragment>
-        ) : (
-          <div className="col-12 md:col-6 lg:col-4 md:ml4 p1">
-            <div className="relative overflow-auto my2">
-              <LocationsSuggestionsCard
-                serviceType={get(orderRef, 'serviceType', PICKUP)}
-              />
-            </div>
           </div>
-        )}
+        </div>
       </main>
     );
   }
