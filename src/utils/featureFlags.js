7d3835b3e7af640ef68ccb8c945eddd0601acba8
Kay Mok <kaymok3@gmail.com>
2019-04-25 11:34:09 -0400

    Add feature flags

---
+++ src/utils/featureFlags.js
@@ -0,0 +1,24 @@
+import environmentIsProd from 'utils/environmentIsProd';
+
+export const FLAGS = {
+  REWARDS: {
+    DEV: true,
+    PROD: false
+  },
+  SHARE_MENU: {
+    DEV: true,
+    PROD: false
+  },
+  FAVORITE_MENU: {
+    DEV: true,
+    PROD: false
+  },
+  CUSTOMER_ADDRESS_BOOK: {
+    DEV: false,
+    PROD: false
+  }
+};
+
+export const isEnabled = flag => {
+  return environmentIsProd() ? flag.PROD : flag.DEV;
+};
