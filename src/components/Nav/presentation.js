ee79a15a2b92d6e1f1441a1b3ed7a2acd66e8dd6
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-02 18:09:48 -0400

    tweak global css values

--- src/components/Nav/presentation.js
+++ src/components/Nav/presentation.js
@@ -5,7 +5,7 @@ import getRoutes from 'utils/getRoutes';
 import { Image, AccountButton, Anchor } from 'components';
 
 const Nav = React.memo(({ brandContext, customer }) => (
-  <div className="Nav fixed t0 l0 z4 col-12 px1_5 py_5 bg-color-white flex justify-between items-center border-color-gray-light">
+  <div className="Nav fixed t0 l0 z4 col-12 px1 md:px1_5 py_5 bg-color-white flex justify-between items-center border-color-gray-light">
     <Anchor url={getRoutes().WELCOME} className="h100 py_25">
       <Image className="h100" src={get(brandContext, 'logoImage')} />
     </Anchor>
