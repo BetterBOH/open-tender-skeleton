2a7115f4ba3d30e042bcab9b191e7ae34dbb2019
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-03 16:41:01 -0400

    move onClose to a status prop listeneer in AccountDetailsEditName

--- src/components/AccountDetailsEditName/index.js
+++ src/components/AccountDetailsEditName/index.js
@@ -1,5 +1,6 @@
 import { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { Status } from 'brandibble-redux';
 import RegistryLoader from 'lib/RegistryLoader';
 import withLocales from 'lib/withLocales';
 import get from 'utils/get';
@@ -32,6 +33,17 @@ class AccountDetailsEditName extends PureComponent {
       firstName: get(props, 'customerAttributes.firstName'),
       lastName: get(props, 'customerAttributes.lastName')
     };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.updateUserStatus === Status.PENDING &&
+      this.props.updateUserStatus === Status.FULFILLED
+    ) {
+      return this.props.onClose();
+    }
+
+    return null;
   }
 
   handleChange = (field, value) => {
@@ -71,12 +83,10 @@ class AccountDetailsEditName extends PureComponent {
     const customerId = get(this, 'props.customerAttributes.id');
     const openTenderRef = get(this, 'props.openTenderRef');
 
-    return this.props
-      .updateUser(openTenderRef, customerId, {
-        first_name: firstName,
-        last_name: lastName
-      })
-      .then(this.props.onClose);
+    return this.props.updateUser(openTenderRef, customerId, {
+      first_name: firstName,
+      last_name: lastName
+    });
   };
 
   render() {
