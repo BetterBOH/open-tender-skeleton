5cee25d2ad0213f60ff9c1de00cd9ad4bdc06a6f
Kay Mok <kaymok3@gmail.com>
2019-05-08 16:16:25 -0400

    Add unique IDs for Bag, Group, Right, and UserCircle svgs

--- src/components/Icon/svgs/Bag.js
+++ src/components/Icon/svgs/Bag.js
@@ -12,15 +12,15 @@ export default ({ fill, alt, uniqueAriaId }) => (
     <desc id={`bag-${uniqueAriaId}`}>{alt || 'Bag'}</desc>
     <defs>
       <path
-        id="bag-a"
+        id={`bag-a-${uniqueAriaId}`}
         d="M6.5,8.5 C6.5,4.91014913 9.41014913,2 13,2 C16.5898509,2 19.5,4.91014913 19.5,8.5 L20,8.5 C21.6568542,8.5 23,9.84314575 23,11.5 L21,21.5 C21,23.1568542 19.6568542,24.5 18,24.5 L8,24.5 C6.34314575,24.5 5,23.1568542 5,21.5 L3,11.5 C3,9.84314575 4.34314575,8.5 6,8.5 L6.5,8.5 Z M9.5,8.5 L16.5,8.5 C16.5,6.56700338 14.9329966,5 13,5 C11.0670034,5 9.5,6.56700338 9.5,8.5 Z"
       />
     </defs>
     <g fill="none" fillRule="evenodd">
-      <mask id="bag-b" fill="#fff">
-        <use xlinkHref="#bag-a" />
+      <mask id={`bag-b-${uniqueAriaId}`} fill="#fff">
+        <use xlinkHref={`#bag-a-${uniqueAriaId}`} />
       </mask>
-      <g fill={fill} mask="url(#bag-b)">
+      <g fill={fill} mask={`url(#bag-b-${uniqueAriaId})`}>
         <rect width="26" height="26" />
       </g>
     </g>
--- src/components/Icon/svgs/Right.js
+++ src/components/Icon/svgs/Right.js
@@ -12,15 +12,15 @@ export default ({ fill, alt, uniqueAriaId }) => (
     <desc id={`right-${uniqueAriaId}`}>{alt || 'Right'}</desc>
     <defs>
       <path
-        id="right-a"
+        id={`right-a-${uniqueAriaId}`}
         d="M16.8786797,14.5 L6.5,14.5 C5.67157288,14.5 5,13.8284271 5,13 C5,12.1715729 5.67157288,11.5 6.5,11.5 L16.8786797,11.5 L13.4393398,8.06066017 C12.8535534,7.47487373 12.8535534,6.52512627 13.4393398,5.93933983 C14.0251263,5.35355339 14.9748737,5.35355339 15.5606602,5.93933983 L21.5606602,11.9393398 C22.1464466,12.5251263 22.1464466,13.4748737 21.5606602,14.0606602 L15.5606602,20.0606602 C14.9748737,20.6464466 14.0251263,20.6464466 13.4393398,20.0606602 C12.8535534,19.4748737 12.8535534,18.5251263 13.4393398,17.9393398 L16.8786797,14.5 Z"
       />
     </defs>
     <g fill="none" fillRule="evenodd">
-      <mask id="right-b" fill="#fff">
-        <use xlinkHref="#right-a" />
+      <mask id={`right-b-${uniqueAriaId}`} fill="#fff">
+        <use xlinkHref={`#right-a-${uniqueAriaId}`} />
       </mask>
-      <g fill={fill} mask="url(#right-b)">
+      <g fill={fill} mask={`url(#right-b-${uniqueAriaId})`}>
         <rect width="26" height="26" />
       </g>
     </g>
