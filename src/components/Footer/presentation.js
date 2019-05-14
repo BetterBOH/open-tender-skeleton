ab0b9b435993362b0ba787ea38c3a46dcb7fce75
Kay Mok <kaymok3@gmail.com>
2019-05-14 12:26:09 -0400

    Update alt tags for logos

--- src/components/Footer/presentation.js
+++ src/components/Footer/presentation.js
@@ -10,7 +10,7 @@ const Footer = React.memo(({ localesContext, brandContext }) => {
     <div className="Footer none md:flex flex-wrap items-center w100 px3 py6 relative none bg-color-brand-color-dark">
       <div className="Footer__logo col-2">
         <Anchor url={getRoutes().WELCOME} className="block md:col-8 lg:col-6">
-          <Image src={logoImage} alt="Logo" />
+          <Image src={logoImage} alt="Home" />
         </Anchor>
       </div>
       <div className="Footer__links col-7 flex justify-around px2">
--- src/components/Nav/presentation.js
+++ src/components/Nav/presentation.js
@@ -7,7 +7,7 @@ import { Image, AccountButton, Anchor } from 'components';
 const Nav = React.memo(({ brandContext, customer }) => (
   <div className="Nav fixed t0 l0 z4 col-12 px1 md:px1_5 py_5 bg-color-white flex justify-between items-center border-color-gray-light">
     <Anchor url={getRoutes().WELCOME} className="h100 py_25">
-      <Image className="h100" src={get(brandContext, 'logoImage')} alt="Logo" />
+      <Image className="h100" src={get(brandContext, 'logoImage')} alt="Home" />
     </Anchor>
     <AccountButton customer={customer} />
   </div>
