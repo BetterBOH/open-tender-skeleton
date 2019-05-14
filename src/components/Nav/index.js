80fece778ba649aeb4ec7f7bd2ce9244acd86835
Kay Mok <kaymok3@gmail.com>
2019-05-14 12:14:02 -0400

    Add alt to nav logo and specify prop in index

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 
-const Nav = React.memo(props =>
-  RegistryLoader(props, 'components.Nav', () => import('./presentation.js'))
+const Nav = React.memo(({ customer }) =>
+  RegistryLoader({ customer }, 'components.Nav', () =>
+    import('./presentation.js')
+  )
 );
 
 export default Nav;
