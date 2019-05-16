3787ac78a6ce7de700625bacbfcb48c3fc2587c7
Kay Mok <kaymok3@gmail.com>
2019-05-16 17:23:14 -0400

    List props and clean up

--- src/components/LineItemRow/index.js
+++ src/components/LineItemRow/index.js
@@ -1,22 +1,29 @@
+import { PureComponent } from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 import PropTypes from 'prop-types';
 import LineItemModel from 'constants/Models/LineItemModel';
 import withLineItemActions from 'lib/withLineItemActions';
 
-const LineItemRow = props => {
-  return RegistryLoader(props, 'components.LineItemRow', () =>
-    import('./presentation.js')
-  );
-};
+class LineItemRow extends PureComponent {
+  static propTypes = {
+    item: LineItemModel.propTypes,
+    isConfigurable: PropTypes.bool
+  };
 
-LineItemRow.propTypes = {
-  item: LineItemModel.propTypes,
-  isConfigurable: PropTypes.bool
-};
+  static defaultProps = {
+    item: null,
+    isConfigurable: true
+  };
 
-LineItemRow.defaultProps = {
-  item: LineItemModel.defaultProps,
-  isConfigurable: true
-};
+  render() {
+    const { item, isConfigurable, updateQuantity } = this.props;
+
+    return RegistryLoader(
+      { item, isConfigurable, updateQuantity },
+      'components.LineItemRow',
+      () => import('./presentation.js')
+    );
+  }
+}
 
 export default withLineItemActions(LineItemRow);
--- src/components/LineItemRow/presentation.js
+++ src/components/LineItemRow/presentation.js
@@ -4,80 +4,84 @@ import currency from 'currency.js';
 
 import { Image, Text, QuantitySpinner } from 'components';
 
-const LineItemRow = React.memo(props => {
-  const { item, isConfigurable, localesContext } = props;
+const LineItemRow = React.memo(
+  ({ item, isConfigurable, updateQuantity, localesContext }) => {
+    const name = get(item, 'productData.name');
+    const quantity = get(item, 'quantity');
+    const calories = get(item, 'productData.nutritional_info.calories');
+    const imageUrl = get(item, 'productData.small_image_url');
 
-  const name = get(item, 'productData.name');
-  const quantity = get(item, 'quantity');
-  const calories = get(item, 'productData.nutritional_info.calories');
-  const imageUrl = get(item, 'productData.small_image_url');
+    const basePrice = get(item, 'productData.price', '0.00');
+    const optionGroupMappings = get(item, 'optionGroupMappings', []);
+    const optionsTotalEffectOnPrice = optionGroupMappings.reduce(
+      (totalEffectOnPrice, currentOptionGroupMapping) =>
+        currency(totalEffectOnPrice).add(
+          get(currentOptionGroupMapping, 'totalEffectOnPrice', '0.00')
+        ),
+      currency('0.00')
+    );
+    const totalPrice = currency(basePrice).add(optionsTotalEffectOnPrice);
 
-  const basePrice = get(item, 'productData.price', '0.00');
-  const optionGroupMappings = get(item, 'optionGroupMappings', []);
-  const optionsTotalEffectOnPrice = optionGroupMappings.reduce(
-    (totalEffectOnPrice, currentOptionGroupMapping) =>
-      currency(totalEffectOnPrice).add(
-        get(currentOptionGroupMapping, 'totalEffectOnPrice', '0.00')
-      ),
-    currency('0.00')
-  );
-  const totalPrice = currency(basePrice).add(optionsTotalEffectOnPrice);
-
-  const presentOptionItems = optionGroupMappings
-    .map(optionGroup =>
-      optionGroup.optionItems.filter(
-        optionItem => optionItem.presence === 'PRESENT'
+    const presentOptionItems = optionGroupMappings
+      .map(optionGroup =>
+        optionGroup.optionItems.filter(
+          optionItem => optionItem.presence === 'PRESENT'
+        )
       )
-    )
-    .flat(2);
-  const optionItemList = presentOptionItems.reduce(
-    (list, optionItem, currentIndex) => {
-      return currentIndex === 0
-        ? `${optionItem.optionItemData.name}`
-        : `${list}, ${optionItem.optionItemData.name}`;
-    },
-    ''
-  );
+      .flat(2);
 
-  return (
-    <div className="LineItemRow flex justify-between items-center py1">
-      <div className="flex items-center">
-        <div className="LineItemRow__image-container flex items-center justify-center radius-md overflow-hidden bg-color-gray-lighter mr1">
-          <Image className="LineItemRow__image" src={imageUrl} alt={name} />
-        </div>
-        <div className="LineItemRow__meta-data flex flex-col">
-          {name && (
-            <Text size="extrasmall" className="text-bold color-black">
-              {name}
-            </Text>
-          )}
-          {optionItemList && (
-            <Text size="extrasmall" className="inline-block color-gray mt_25">
-              {optionItemList}
-            </Text>
-          )}
-          <div className="flex mt_25">
-            <Text size="extrasmall" className="text-bold color-gray-dark mr_5">
-              {currency(totalPrice, {
-                formatWithSymbol: true
-              }).format()}
-            </Text>
-            {calories && (
-              <Text size="extrasmall" className="color-gray">
-                {`${calories} ${localesContext.Language.t('menu.cal')}`}
+    const optionItemList = presentOptionItems.reduce(
+      (list, optionItem, currentIndex) => {
+        return currentIndex === 0
+          ? `${optionItem.optionItemData.name}`
+          : `${list}, ${optionItem.optionItemData.name}`;
+      },
+      ''
+    );
+
+    return (
+      <div className="LineItemRow flex justify-between items-center py1">
+        <div className="flex items-center">
+          <div className="LineItemRow__image-container flex items-center justify-center radius-md overflow-hidden bg-color-gray-lighter mr1">
+            <Image className="LineItemRow__image" src={imageUrl} alt={name} />
+          </div>
+          <div className="LineItemRow__meta-data flex flex-col">
+            {name && (
+              <Text size="extrasmall" className="text-bold color-black">
+                {name}
               </Text>
             )}
+            {optionItemList && (
+              <Text size="extrasmall" className="color-gray mt_25">
+                {optionItemList}
+              </Text>
+            )}
+            <div className="flex mt_25">
+              <Text
+                size="extrasmall"
+                className="text-bold color-gray-dark mr_5"
+              >
+                {currency(totalPrice, {
+                  formatWithSymbol: true
+                }).format()}
+              </Text>
+              {calories && (
+                <Text size="extrasmall" className="color-gray">
+                  {`${calories} ${localesContext.Language.t('menu.cal')}`}
+                </Text>
+              )}
+            </div>
           </div>
         </div>
+        <QuantitySpinner
+          isDisabled={!isConfigurable}
+          quantity={quantity}
+          handleDecrement={() => updateQuantity(quantity, quantity - 1)}
+          handleIncrement={() => updateQuantity(quantity, quantity + 1)}
+        />
       </div>
-      <QuantitySpinner
-        isDisabled={!isConfigurable}
-        quantity={quantity}
-        handleDecrement={() => props.updateQuantity(quantity, quantity - 1)}
-        handleIncrement={() => props.updateQuantity(quantity, quantity + 1)}
-      />
-    </div>
-  );
-});
+    );
+  }
+);
 
 export default LineItemRow;
