fbefd4f24227f5b01e78de8436ac5b446ea136ab
Ellis Marte <elliscmarte@gmail.com>
2019-05-16 15:53:48 -0400

    use switchToPickup action

--- src/components/LocationsSearchResults/index.js
+++ src/components/LocationsSearchResults/index.js
@@ -1,20 +1,37 @@
 import { PureComponent } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import RegistryLoader from 'lib/RegistryLoader';
 import get from 'utils/get';
 import getLocationSlug from 'utils/getLocationSlug';
 import getRoutes, { RouteProperties } from 'utils/getRoutes';
+import { confirmChangeToPickup } from 'state/actions/serviceTypeActions';
+import { FULFILLED, PENDING } from 'constants/Status';
 
 class LocationsSearchResults extends PureComponent {
+  state = {
+    location: null
+  };
+
+  componentDidUpdate(prevProps) {
+    if (
+      prevProps.confirmChangeToPickupStatus === PENDING &&
+      this.props.confirmChangeToPickupStatus === FULFILLED
+    ) {
+      const { history } = this.props;
+
+      const basename = getRoutes(RouteProperties.BASENAME).MENUS;
+      const locationSlug = getLocationSlug(this.state.location);
+
+      return history.push(`${basename}/${locationSlug}`);
+    }
+  }
+
   onSelect = location => {
-    const { history } = this.props;
-
-    const basename = getRoutes(RouteProperties.BASENAME).MENUS;
-    const locationSlug = getLocationSlug(location);
-
-    return history.push(`${basename}/${locationSlug}`);
+    this.setState({ location });
+    this.props.actions.confirmChangeToPickup(location);
   };
 
   render() {
@@ -34,7 +51,20 @@ class LocationsSearchResults extends PureComponent {
 
 const mapStateToProps = state => ({
   geolocations: get(state, 'openTender.data.geolocations'),
-  userCoordinates: get(state, 'geocoder.userCoordinates')
+  userCoordinates: get(state, 'geocoder.userCoordinates'),
+  confirmChangeToPickupStatus: get(state, 'status.confirmChangeToPickup')
 });
 
-export default connect(mapStateToProps)(withRouter(LocationsSearchResults));
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    {
+      confirmChangeToPickup
+    },
+    dispatch
+  )
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(LocationsSearchResults));
