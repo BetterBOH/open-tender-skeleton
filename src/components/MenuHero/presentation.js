d981bbfa15ac5696e54d2c8593c504e5f14e780b
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-20 19:23:36 -0400

    add menu transitions (#458)

--- src/components/MenuHero/presentation.js
+++ src/components/MenuHero/presentation.js
@@ -7,7 +7,7 @@ const MenuHero = React.memo(({ location }) => {
 
   return (
     <div className="MenuHero bg-color-white relative z3">
-      <div className="flex flex-col md:flex-col-reverse">
+      <div className="MenuHero__image flex flex-col md:flex-col-reverse">
         <HeroImage src={heroImage} />
       </div>
       <div className="MenuHero__location-card absolute flex justify-center col-12 md:col-6 lg:col-4 px1">
