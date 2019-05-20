ce53d08a680975207fa2feae253a8dfe3934925a
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-20 13:31:27 -0400

    QA: Adjust overlay colors, add transition to modals (#452)
    * change modal background color, add transition to modal in
    
    * iron out modal transitions
    
    * add white-wash color for cards

--- src/components/Modal/presentation.js
+++ src/components/Modal/presentation.js
@@ -13,20 +13,28 @@ import {
 } from 'components';
 
 class Modal extends Component {
+  onClose = () => {
+    const { variant, actions } = this.props;
+    switch (variant) {
+      case ModalTypes.LINE_ITEM_EDITOR:
+        return get(getConfig(ConfigKeys.STATE), 'history').goBack;
+      default:
+        return actions.resetModal;
+    }
+  };
+
   renderModalInner = () => {
-    const { variant, data, actions, modalIsFrozen } = this.props;
+    const { variant, data, modalIsFrozen } = this.props;
+
+    const onCloseFn = this.onClose();
 
     switch (variant) {
       case ModalTypes.LINE_ITEM_EDITOR:
-        return (
-          <LineItemEditor
-            onClose={get(getConfig(ConfigKeys.STATE), 'history').goBack}
-          />
-        );
+        return <LineItemEditor onClose={onCloseFn} />;
       case ModalTypes.MENU_NAVIGATION:
-        return <MenuNavigationLinks onClose={actions.resetModal} data={data} />;
+        return <MenuNavigationLinks onClose={onCloseFn} data={data} />;
       case ModalTypes.MENU_FILTER:
-        return <MenuFilters onClose={actions.resetModal} />;
+        return <MenuFilters onClose={onCloseFn} />;
       case ModalTypes.INVALID_ITEMS_IN_CART:
         return (
           <InvalidItemsInCart
@@ -41,17 +49,23 @@ class Modal extends Component {
   };
 
   render() {
-    const { modalIsActive, modalIsFrozen, actions } = this.props;
+    const { modalIsActive, modalIsFrozen } = this.props;
 
     if (!modalIsActive) return null;
 
+    const onCloseFn = this.onClose();
+
     return (
       <div className="Modal fixed t0 r0 b0 l0 flex z5">
-        <div className="Modal__inner z1">{this.renderModalInner()}</div>
-        <div
-          className="Modal__overlay absolute vh100 col-12 bg-color-white-overlay"
-          onClick={!modalIsFrozen ? actions.resetModal : f => f}
-        />
+        <div className="Modal__inner col-12 relative">
+          <div
+            className="Modal__overlay absolute vh100 col-12 bg-color-white-overlay z1"
+            onClick={!modalIsFrozen ? onCloseFn : f => f}
+          />
+          <div className="Modal__content relative z2">
+            {this.renderModalInner()}
+          </div>
+        </div>
       </div>
     );
   }
