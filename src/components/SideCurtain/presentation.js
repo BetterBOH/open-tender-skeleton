aa27b03101497812014c9214d6c7e297306ebc44
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-20 14:53:21 -0400

    add react-transition-groups, transitionify MiniCart

--- src/components/SideCurtain/presentation.js
+++ src/components/SideCurtain/presentation.js
@@ -1,46 +1,39 @@
-import React from 'react';
-import cx from 'classnames';
-import SideCurtainVariants from 'constants/SideCurtainVariants';
-import { MiniCart } from '../index';
+import React, { PureComponent } from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-const { MINI_CART } = SideCurtainVariants;
+import { TRANSITION_TIMING } from 'constants/Presentation';
+import { MiniCart } from 'components';
 
-const SideCurtain = React.memo(
-  ({ sideCurtainIsActive, variant, resetSideCurtain }) => {
-    const renderSideCurtainInner = () => {
-      switch (variant) {
-        case MINI_CART:
-          return <MiniCart handleClose={resetSideCurtain} />;
-        default:
-          return null;
-      }
-    };
+class SideCurtain extends PureComponent {
+  render() {
+    const { sideCurtainIsActive, resetSideCurtain } = this.props;
 
     return (
-      <div
-        className={cx('SideCurtain r0 b0 l0 fixed z4 events-none', {
-          'SideCurtain--isActive events-all': sideCurtainIsActive
-        })}
+      <CSSTransition
+        in={sideCurtainIsActive}
+        classNames={{
+          enter: 'SideCurtain--enter',
+          enterDone: 'SideCurtain--entered',
+          exit: 'SideCurtain--exit',
+          exitDone: 'SideCurtain--exited'
+        }}
+        timeout={TRANSITION_TIMING}
       >
-        <div
-          className={cx(
-            'SideCurtain__drawer relative bg-color-white z1 h100 w100 mlauto',
-            {
-              'SideCurtain__drawer--isActive shadow-md': sideCurtainIsActive
-            }
-          )}
-        >
-          {renderSideCurtainInner()}
+        <div className="SideCurtain r0 b0 l0 fixed z4">
+          <div className="SideCurtain__drawer relative bg-color-white z1 h100 w100 mlauto">
+            <MiniCart
+              miniCartIsActive={sideCurtainIsActive}
+              handleClose={resetSideCurtain}
+            />
+          </div>
+          <div
+            onClick={resetSideCurtain}
+            className="SideCurtain__overlay bg-color-white-overlay absolute t0 r0 b0 l0"
+          />
         </div>
-        <div
-          onClick={resetSideCurtain}
-          className={cx('SideCurtain__overlay absolute t0 r0 b0 l0 opacity-0', {
-            'SideCurtain__overlay--isActive opacity-1 bg-color-white-overlay': sideCurtainIsActive
-          })}
-        />
-      </div>
+      </CSSTransition>
     );
   }
-);
+}
 
 export default SideCurtain;
