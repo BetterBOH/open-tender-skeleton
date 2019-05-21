0952d10f5f962c7201922670b0f02666e0219b16
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-21 17:52:24 -0400

    add brand to Navigation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import RegistryLoader from 'lib/RegistryLoader';
 
-const Nav = React.memo(({ customer }) =>
-  RegistryLoader({ customer }, 'components.Nav', () =>
+const Nav = React.memo(({ customer, brand }) =>
+  RegistryLoader({ customer, brand }, 'components.Nav', () =>
     import('./presentation.js')
   )
 );
--- src/components/Nav/presentation.js
+++ src/components/Nav/presentation.js
@@ -3,12 +3,12 @@ import get from 'utils/get';
 import getRoutes from 'utils/getRoutes';
 import { Image, AccountButton, Anchor } from 'components';
 
-const Nav = React.memo(({ customer, brandContext, localesContext }) => (
+const Nav = React.memo(({ customer, brand, localesContext }) => (
   <div className="Nav fixed t0 l0 z4 col-12 px1 md:px1_5 py_5 bg-color-white flex justify-between items-center border-bottom">
     <Anchor url={getRoutes().WELCOME} className="h100 py_25">
       <Image
         className="h100"
-        src={get(brandContext, 'logoImage')}
+        src={get(brand, 'logo')}
         alt={localesContext.Language.t('global.home')}
       />
     </Anchor>
