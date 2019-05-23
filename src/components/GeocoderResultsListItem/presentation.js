868345d60e480839113dd81c2bb6572745289bcb
Joshie Fishbein <joshie@sanctuary.computer>
2019-05-23 14:42:34 -0400

    tweaks styles for MapboxGeocoder and GeocoderResultsList

--- src/components/GeocoderResultsListItem/presentation.js
+++ src/components/GeocoderResultsListItem/presentation.js
@@ -11,13 +11,16 @@ const GeocoderResultsListItem = React.memo(({ option, onSelect }) => {
         className="flex flex-wrap w100"
         onClick={() => onSelect(option.value)}
       >
-        <Icon className="GeocoderResultsListItem__icon m1" icon="Marker" />
+        <Icon
+          className="GeocoderResultsListItem__icon m1 pt_25"
+          icon="Marker"
+        />
         <div className="col-9 py1">
-          <Text size="detail" className="text-semibold block mb_25">
+          <Text size="detail" className="text-bold color-black block mb_25">
             {meta.address && `${meta.address} `}
             {meta.street}
           </Text>
-          <Text size="detail" className="color-gray-dark">
+          <Text size="detail" className="text-semibold color-gray">
             {meta.city}, {meta.state}, {meta.country}
           </Text>
         </div>
