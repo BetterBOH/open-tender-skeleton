f8d19c72a6fe5aeaa641f59f9060d92e460a7ebf
Joshie Fishbein <joshie@sanctuary.computer>
2019-06-06 14:35:16 -0400

    add error catch for expired customer tokens

--- src/state/actions/applicationActions.js
+++ src/state/actions/applicationActions.js
@@ -1,8 +1,21 @@
-import { setupBrandibbleRedux as setupOpenTenderRedux } from 'brandibble-redux';
+import {
+  setupBrandibbleRedux as setupOpenTenderRedux,
+  unauthenticateUser
+} from 'brandibble-redux';
 
 export const INITIALIZE_APPLICATION = 'INITIALIZE_APPLICATION';
 export const initializeApplication = OpenTenderRef => dispatch =>
   dispatch({
     type: INITIALIZE_APPLICATION,
-    payload: dispatch(setupOpenTenderRedux(OpenTenderRef))
+    payload: dispatch(setupOpenTenderRedux(OpenTenderRef)).catch(errors => {
+      errors.forEach(error => {
+        switch (error.code) {
+          case 'customer_token.expired':
+            dispatch(unauthenticateUser(OpenTenderRef));
+            break;
+          default:
+            break;
+        }
+      });
+    })
   });
