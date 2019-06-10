8b9c500eb387386f5288aaee3d5a5277096fb924
Kay Mok <kaymok3@gmail.com>
2019-06-10 15:11:23 -0400

    Clean up spinner classes

--- src/components/Loader/presentation.js
+++ src/components/Loader/presentation.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loader = React.memo(() => (
   <div className="Loader vh100 vw100 flex justify-center items-center">
-    <div className="Spinner Spinner--loader" />
+    <div className="Spinner__icon Spinner__icon--large" />
   </div>
 ));
 
--- src/components/Spinner/presentation.js
+++ src/components/Spinner/presentation.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Spinner = React.memo(() => (
-  <div className="Spinner__container col-12 h100 flex justify-center items-center">
-    <div className="Spinner" />
+  <div className="Spinner col-12 h100 flex justify-center items-center">
+    <div className="Spinner__icon" />
   </div>
 ));
 
