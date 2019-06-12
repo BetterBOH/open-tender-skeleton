d081362356c39b179f7de77d5e3d0dd3768c6e46
Kay Mok <kaymok3@gmail.com>
2019-06-12 16:14:21 -0400

    Dashboard mobile QA

--- src/components/FavoriteItem/presentation.js
+++ src/components/FavoriteItem/presentation.js
@@ -20,59 +20,64 @@ const FavoriteItem = React.memo(
     const calories = get(menuItemDetails, 'nutritional_info.calories');
 
     return (
-      <div className="FavoriteItem mr1">
-        <div className="mb1 relative pr1">
-          <div className="FavoriteItem__image-container flex justify-center items-center shadow-sm radius-md overflow-hidden bg-color-gray-lighter">
+      <div className="FavoriteItem flex flex-col justify-between mr1">
+        <div className="FavoriteItem__meta">
+          <div className="FavoriteItem__image-container flex justify-center items-center shadow-sm radius-md overflow-hidden bg-color-gray-lighter mb1 pr1">
             <Image
               src={`${IMAGE_PREFIX}${imageUrl}`}
               className="FavoriteItem__image bg-color-gray shadow-md radius-sm"
             />
           </div>
-        </div>
-        <div className="FavoriteItem__details py_5 overflow-hidden text-overflow-ellipsis">
-          <Text size="small" className="nowrap block text-bold color-black">
-            {menuItemName}
-          </Text>
-          {(!!price || !!calories) && (
+          <div className="FavoriteItem__details py_5 overflow-hidden text-overflow-ellipsis">
             <Text
               size="small"
-              className="nowrap inline-block color-gray text-semibold capitalize"
+              className="nowrap inline-block text-bold color-black"
             >
-              {!!price &&
-                currency(price, {
-                  formatWithSymbol: true
-                }).format()}{' '}
-              {!!calories && (
-                <span>
-                  {parseFloat(calories).toFixed()}{' '}
-                  {localesContext.Language.t(
-                    'favorites.nutritionFactUnits.cal'
-                  )}
-                </span>
-              )}
+              {menuItemName}
             </Text>
+            {(!!price || !!calories) && (
+              <Text
+                size="small"
+                className="nowrap block color-gray text-semibold capitalize"
+              >
+                {!!price &&
+                  currency(price, {
+                    formatWithSymbol: true
+                  }).format()}{' '}
+                {!!calories && (
+                  <span>
+                    {parseFloat(calories).toFixed()}{' '}
+                    {localesContext.Language.t(
+                      'favorites.nutritionFactUnits.cal'
+                    )}
+                  </span>
+                )}
+              </Text>
+            )}
+          </div>
+        </div>
+        <div className="FavoriteItem__add-to-cart-button">
+          {!!get(item, 'option_groups.length', 0) ? (
+            <Button
+              variant="secondary"
+              className="bg-color-gray-dark hover-bg-color-black flex px1"
+              onClick={handleClickIncrement}
+            >
+              <Text
+                size="extra-small"
+                className="color-white uppercase text-bold letter-spacing-sm"
+              >
+                {localesContext.Language.t('menu.add')}
+              </Text>
+            </Button>
+          ) : (
+            <QuantitySpinner
+              quantity={quantity}
+              handleIncrement={handleClickIncrement}
+              handleDecrement={handleClickDecrement}
+            />
           )}
         </div>
-        {!!get(item, 'option_groups.length', 0) ? (
-          <Button
-            variant="secondary"
-            className="bg-color-gray-dark hover-bg-color-black flex px1"
-            onClick={handleClickIncrement}
-          >
-            <Text
-              size="extra-small"
-              className="color-white uppercase text-bold letter-spacing-sm"
-            >
-              {localesContext.Language.t('menu.add')}
-            </Text>
-          </Button>
-        ) : (
-          <QuantitySpinner
-            quantity={quantity}
-            handleIncrement={handleClickIncrement}
-            handleDecrement={handleClickDecrement}
-          />
-        )}
       </div>
     );
   }
